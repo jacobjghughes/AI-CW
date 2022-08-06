b83da0cb492b4a8920d63348c248e76735922e0d
jakey <jacobjghughes@ymail.com>
2022-08-07 00:33:17 +0100

    Added part 2

--- cw_wp_1833809.pl
+++ cw_wp_1833809.pl
@@ -3,16 +3,53 @@ actor_has_link(L,A) :-
     actor(A), wp(A,WT), wt_link(WT,L).
 
 % Attempt to solve by visiting each oracle in ID order
-eliminate(As,A,K) :- 
-    As=[A], !
+eliminate(Actors,Actor, VisitedOracles) :- 
+    Actors=[Actor], !
     ;
-    solve_task(find(o(K)),_), !,
-    my_agent(N),
-    agent_ask_oracle(N,o(K),link,L), 
-    include(actor_has_link(L),As,ViableAs), 
-    K1 is K+1, 
-    eliminate(ViableAs,A,K1).
+    my_agent(Agent),  
+    get_agent_energy(Agent, AgentEnergy), 
+    ailp_grid_size(GridSize),
+    get_agent_position(Agent, Pos),
+    EnergyMax is GridSize * GridSize / 4,
+    EnergyAsk is GridSize / 10,
+    
+    %find nearest oracle not visited
+    solve_task_astar(find(o(NextOracle)), [_:Pos:[]], [], [Pos|Path]),
+    \+ member(NextOracle, VisitedOracles),
+    \+ agent_check_oracle(Agent, o(NextOracle)), 
+    !,
+    (
+        % if agent energy after the task is above the threshold of 25% max energy then we can proceed otherwise find a charger
+        % 25% seems to work well from testing random maps
+        length(Path, PathCost),
+        (AgentEnergy - PathCost - EnergyAsk) >= EnergyMax*0.25,
 
+        agent_do_moves(Agent, Path),
+        agent_ask_oracle(Agent, o(NextOracle), link, Ans),
+        NewVisited = [NextOracle | VisitedOracles], 
+        include(actor_has_link(Ans), Actors, ViableAs),
+        eliminate(ViableAs, Actor, NewVisited)
+        ;
+
+        solve_task(find(c(Charger)),_),
+        agent_topup_energy(Agent, c(Charger)),
+        get_agent_position(Agent, NewPos),
+        solve_task_astar(find(o(NewNextOracle)), [_:NewPos:[]], [], [NewPos|NewPath]),
+        \+ member(NewNextOracle, VisitedOracles),
+        \+ agent_check_oracle(Agent, o(NewNextOracle)),
+        !,
+        agent_do_moves(Agent,NewPath),
+        agent_ask_oracle(Agent, o(NewNextOracle), link, Ans),
+        NewVisited = [NewNextOracle | VisitedOracles], 
+        include(actor_has_link(Ans), Actors, ViableAs),
+        eliminate(ViableAs, Actor, NewVisited)
+    );
+    % if we fail to find an available oracle then the task fails and the actor is unknown
+    Actor = unknown
+    .
+        
 % Deduce the identity of the secret actor A
 find_identity(A) :- 
-    findall(A,actor(A),As), eliminate(As,A,1).
+    findall(A,actor(A),As), eliminate(As,A,[])
+    ;
+    A = unknown.
